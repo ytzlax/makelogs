81316458b1d0860b0020e276010b5921ba399d66
https://github.com/ytzlax <ytzlax@gmail.com>
2018-01-15 21:59:08 +0200

    Add actions

---
+++ samples/actions.js
@@ -0,0 +1,16 @@
+module.exports = [
+{name: "create",file_name:"users"},
+{name: "preview",file_name:"goals"},
+{name: "edit",file_name:"users"},
+{name: "delete",file_name:"users"},
+{name: "share",file_name:"users"},
+{name: "revoke share",file_name:"users"},
+{name: "reshare",file_name:"goals"},
+{name: "public share",file_name:"goals"},
+{name: "tag",file_name:"goals"},
+{name: "comment",file_name:"goals"},
+{name: "favorites",file_name:"users"},
+{name: "upload",file_name:"users"},
+{name: "download",file_name:"users"},
+{name: "rename",file_name:"users"}
+]
